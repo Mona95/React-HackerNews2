95fef686f1609b22c9a18bacfadad20e2993fbc2
39981838 <shamona9594@gmail.com>
2019-12-28 09:48:33 +0330

    snapshot tests added

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App, {Search, Button, Table} from './components/App';
 
 describe('App', () => {
   
@@ -18,4 +18,55 @@ describe('App', () => {
     expect(tree).toMatchSnapshot();
   });
 });
+
+describe('Search', () => {
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<Search/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Search/>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot(); 
+  });
+});
+
+describe('Button', () => {
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<Button/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Button/>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot(); 
+  });
+});
+
+describe('Table', () => {
+  const props = {
+          list: [
+            { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+            { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+          ],
+        };
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<Table {...props}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Table {...props}/>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
 
